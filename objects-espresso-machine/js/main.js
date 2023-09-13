09a3a4bbb3f5b627e71f5eafe82f8698586a9a38
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-13 19:35:43 +0100

    created methods for esspresomachine

--- objects-espresso-machine/js/main.js
+++ objects-espresso-machine/js/main.js
@@ -1,13 +1,22 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
 
 class ExpressoMachine {
-  constructor(name, modelNumber, yearMft,color) {
+  constructor(name, modelNumber, yearMft, color, boilingTime) {
     this.name = name;
     this.modelNumber = modelNumber;
-      this.yearMft = yearMft;
-      this.color = color
+    this.yearMft = yearMft;
+    this.color = color;
+    this.boilingTime = boilingTime;
   }
-    
-    
+
+  boilWater() {
+    if (this.boilingTime === 200) {
+      console.log(`water is boiled`);
+      releaseSteam();
+    }
+  }
+  releaseSteam() {
+    console.log(`close door`);
+  }
     
 }
