a74903bb856d74b367729c3a680ac69a1822a021
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-13 19:26:00 +0100

    added constructor variables

--- objects-espresso-machine/js/main.js
+++ objects-espresso-machine/js/main.js
@@ -1,2 +1,9 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
 
+class ExpressoMachine {
+  constructor(name, modelNumber, yearMft) {
+    this.name = name;
+    this.modelNumber = modelNumber;
+    this.yearMft = yearMft;
+  }
+}
