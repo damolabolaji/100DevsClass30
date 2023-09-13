a29e3ccb4d20c1a1a649b7b8a8de3a69691d92c2
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-13 19:26:49 +0100

    added 4th property

--- objects-espresso-machine/js/main.js
+++ objects-espresso-machine/js/main.js
@@ -1,9 +1,13 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
 
 class ExpressoMachine {
-  constructor(name, modelNumber, yearMft) {
+  constructor(name, modelNumber, yearMft,color) {
     this.name = name;
     this.modelNumber = modelNumber;
-    this.yearMft = yearMft;
+      this.yearMft = yearMft;
+      this.color = color
   }
+    
+    
+    
 }
